d55d9163e78db78b6ec6eca230bec5939ff4036e
mikael <mikael.fangel@pm.me>
2022-11-02 19:34:03 +0100

    Added functions to get words from files
    Added a function count how many lines there is in a file to ensure our
    random passphrase is within the files and added intial codes to retriev
    a word from the file.

--- discover.c
+++ discover.c
@@ -1,10 +1,21 @@
 #include "common.h"
-#include <string.h>
+#include <stdio.h>
 #define MAX_PASS_LENGTH 1024
+#define MAX_PATH_LENGTH 1024
+#define RESOURCE_PATH ./resources/
+#define RESOURCE_FILES 3
 
 static char *server_pass_phrase;
+static char *gen_files[] = {"adjective_list.txt", "animal_list.txt", "verb_list.txt"};
+int count_lines_in_file(FILE *fptr);
+char *get_line_from_file(int resource_file_number);
 
-bool authenticate(char *phrase) 
+void seed_rand(void) 
+{
+    srand(time(0));
+}
+
+bool validate_pass_phrase(char *phrase) 
 {
     return strncmp(phrase, server_pass_phrase, strnlen(server_pass_phrase, MAX_PASS_LENGTH)) == 0;
 }
@@ -12,6 +23,11 @@ bool authenticate(char *phrase)
 char *generate_pass_phrase(void) 
 {
     free_pass_phrase();
+    
+    for(int i = 0; i < RESOURCE_FILES; i++)
+        {
+            
+        }
     server_pass_phrase = (char *) malloc(sizeof(char));
     return server_pass_phrase;
 }
@@ -23,4 +39,44 @@ void free_pass_phrase(void)
         free(server_pass_phrase);
         server_pass_phrase = NULL;
     }
+}
+
+char *get_line_from_file(int resource_file_number)
+{
+    char source[MAX_PATH_LENGTH];
+    strncat(source, gen_files[resource_file_number], MAX_PATH_LENGTH);
+    FILE *fptr = fopen(source, "r");
+    
+    char *line = NULL;
+    size_t length = 0;
+    ssize_t nread;
+
+    // Make sure that the line we try to get is within the length of the file
+    int lines = count_lines_in_file(fptr);
+    int line_to_get;
+    if(lines > 0)
+        line_to_get = rand() % lines;
+    else
+        goto error;
+
+    for(int i = 0; i < line_to_get; i++)
+        nread = getline(&line, &length, fptr);
+
+    error:
+        fclose(fptr);
+        return line;
+}
+
+int count_lines_in_file(FILE *fptr)
+{
+    if(!fptr)
+        return -1;
+    
+    char c;
+    int lines = 0;
+    while((c = fgetc(fptr)) != EOF)
+        if(c == '\n')
+            lines++;
+
+    return lines;
 }
