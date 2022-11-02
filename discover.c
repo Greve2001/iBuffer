aa7e89bd0363e04784d43a71a8e1c1da5956aeeb
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-11-02 16:57:56 +0100

    added simple suggestion to auth function and started gen pass phrase

--- discover.c
+++ discover.c
@@ -1,9 +1,26 @@
 #include "common.h"
+#include <string.h>
+#define MAX_PASS_LENGTH 1024
 
-bool authenticate(char *phrase) {
-    return false;
+static char *server_pass_phrase;
+
+bool authenticate(char *phrase) 
+{
+    return strncmp(phrase, server_pass_phrase, strnlen(server_pass_phrase, MAX_PASS_LENGTH)) == 0;
 }
 
-char *generate_pass_phrase(void) {
-    return "";
+char *generate_pass_phrase(void) 
+{
+    free_pass_phrase();
+    server_pass_phrase = (char *) malloc(sizeof(char));
+    return server_pass_phrase;
+}
+
+void free_pass_phrase(void)
+{
+    if (server_pass_phrase) 
+    {
+        free(server_pass_phrase);
+        server_pass_phrase = NULL;
+    }
 }
