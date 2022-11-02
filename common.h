26b79e59d1fea7c9406cbb254386d26e9b92174f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-11-02 19:34:03 +0100

    Added functions to get words from files
    Added a function count how many lines there is in a file to ensure our
    random passphrase is within the files and added intial codes to retriev
    a word from the file.

--- common.h
+++ common.h
@@ -1,7 +1,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-bool authenticate(char *);
+bool validate_pass_phrase(char *phrase);
 char *generate_pass_phrase(void);
 void free_pass_phrase(void);
