d6780e94d2971ee1fb8916f9fa6a415ac0ec1ac7
Christian <s133288@dtu.dk>
2022-11-03 10:47:36 +0100

    working on changing from array to linked list

--- linklist.c
+++ linklist.c
@@ -5,18 +5,18 @@
 
 // buffered lines, that are currently not touched
 static int size = 0;
-static line* first_line;
-static line* last_line;
+static Line* first_line;
+static Line* last_line;
 
 
 // active lines
 static int active_users = 0;
-static active_line* active_first_line;
-static active_line* active_last_line;
+static Active_Line* active_first_line;
+static Active_Line* active_last_line;
 
 //this users active line
 static int users_active_line_length = 0;
-static active_line* users_active_line;
+static Active_Line* users_active_line;
 
 
 /**********************************************************
@@ -24,7 +24,7 @@ static active_line* users_active_line;
 *
 ************************************************************/
 void make_new_line(void){
-	line* new = (line*) malloc(sizeof(line));
+	Line* new = (Line*) malloc(sizeof(Line));
 
 	if(!new){
 		//TODO: error handling for malloc error
@@ -41,14 +41,14 @@ void make_new_line(void){
 * Theses methods are used to manipulate the active lines
 *
 ***************************************************************/
-void active_line_size(active_line* my_line){
-	letter* current_char = my_line->first_char;
+void active_line_size(Active_Line* my_line){
+	Letter* current_char = my_line->first_char;
 	
 }
 
 
 void make_line_active(int line_number){
-	line* line_to_load = first_line;
+	Line* line_to_load = first_line;
 	for(int i = 0; i < line_number; i++){
 		line_to_load = line_to_load->next;
 	}
@@ -60,16 +60,19 @@ void make_line_active(int line_number){
 }
 
 
-void edit_line(void){
+void list_line_for_edit(Line* li){
+	Active_Line* new_line= malloc(sizeof(Active_Line));
+	li->active_line = new_line;
+	new_line->original_line = li;
 }
 
-void delist_line_for_edit(active_line* line){
+void delist_line_for_edit(Active_Line* line){
 	//allocate memory for the new array, and copy over each letter to it
 	char* delisted_line = malloc((users_active_line_length + 1) * sizeof(char));
-	letter* current_letter = line->first_char;
+	Letter* current_letter = line->first_char;
 	for(int i = 0; i < users_active_line_length; i++){
 		delisted_line[i] = current_letter->character;
-		letter* temp = current_letter;
+		Letter* temp = current_letter;
 		current_letter = current_letter->next;
 		free(temp);
 	}
@@ -80,30 +83,44 @@ void delist_line_for_edit(active_line* line){
 	line->original_line->paragraph = delisted_line;		//saves the new array to the inactive list
 	
 	//Then we need to remove the active line from the linked list of active lines
-	active_line* current_active_line = active_first_line;
+	Active_Line* current_active_line = active_first_line;
 	for(;current_active_line->next != line; current_active_line = current_active_line->next){
 		;
 	}
 	current_active_line->next = line->next;
+	line->original_line->active_line = NULL;
 	
 	//finaly we can free up our original line.
 	free(line);
-	
-	
 }
 
 
 /******************************************************************
-* These methods need public accessability.
-*
+* These methods need public accessability on the local machine
+* 
 ******************************************************************/
-void enable_line_for_edit(int line_number, bool local_flag){
-	if (local_flag){
-		if (users_active_line){
-			delist_line_for_edit(users_active_line);
-			//TODO: make TCP sent a delist to the other clients
-		}
-		
-		
+void enable_line_for_edit(int line_number){
+	if (users_active_line){
+		delist_line_for_edit(users_active_line);
+		//TODO: make TCP sent a delist to the other clients
 	}
+	Line* pointer_to_line = first_line;
+	for(int i = 0; i < line_number; i++){
+		pointer_to_line = pointer_to_line->next;
+	}
+	list_line_for_edit(pointer_to_line);
+		
 }
+
+void delete_in_line(int position){
+	;//TODO: delete version
+}
+
+void insert_in_line(int position, char character){
+	;//TODO: insert a letter (might be the last)
+}
+
+/***********
+* Methods for the TCP connection
+*
+***********/
--- linklist.h
+++ linklist.h
@@ -1,20 +1,22 @@
+struct Active_Line; //Forward decleration
 
 //for saving the individual lines
 typedef struct Lines{
 	struct Lines* next;
+	struct Active_Line* active_line;
 	char* paragraph;
-} line;
+} Line;
 
 
 //for saving the active lines
 typedef struct Letters{
 	struct Letters* next;
 	char character;
-} letter;
+} Letter;
 
 
 typedef struct Active_Line{
 	struct Active_Line* next;	//pointer to next data in the linked list
-	line* original_line;		//used to either delete or save it when finished editing
-	letter* first_char;			//the first char the paragraph
-} active_line;
+	Line* original_line;		//used to either delete or save it when finished editing
+	Letter* first_char;			//the first char the paragraph
+} Active_Line;
