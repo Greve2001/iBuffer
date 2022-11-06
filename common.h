79e2af31e1bb5bc6d6e29b18fa44de665bb65cdb
mikael <mikael.fangel@pm.me>
2022-11-06 18:36:50 +0100

    Added initial code for sending a broadcast message
    The function for sending a broadcast have been tested with wireshark and
    sends out a broadcast using the broadcast adresse 255.255.255.255 with
    the message EHLO iBuffer.

--- common.h
+++ common.h
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <time.h>
 
-bool validate_pass_phrase(char *phrase);
+bool validate_pass_phrase(char *);
+char *send_udp_broadcast(void);
+void listen_udp_broadcat(void);
 char *generate_pass_phrase(void);
+char *get_line_from_file(int, size_t *);
 void free_pass_phrase(void);
+int count_lines_in_file(FILE *);
