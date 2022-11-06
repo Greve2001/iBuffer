645c43550bd958076d2d86926c670b80d755225a
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-11-06 19:44:32 +0100

    Intial server code added
    The code for a simple UDP server has been added and have been tested with netcat. It can recieved a string from netcat but fails to get messages sent via broadcast.

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 
 bool validate_pass_phrase(char *);
 char *send_udp_broadcast(void);
-void listen_udp_broadcat(void);
+void listen_udp_broadcast(void);
 char *generate_pass_phrase(void);
 char *get_line_from_file(int, size_t *);
 void free_pass_phrase(void);
