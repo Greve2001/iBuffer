13daee732652aa0abd4d59eb917d6ac416fc4555
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-11-09 14:31:30 +0100

    Made startupWindow for application launch

--- tui.c
+++ tui.c
@@ -12,6 +12,7 @@ void interpretChar2(char c, char* str, int* pos);
 void moveCursor(char c);
 void startTUI();
 void stopTUI();
+int startupWindow();
 
 #define MAX_STRING_LENGTH   99
 #define CHAR_RANGE_START    32
@@ -30,9 +31,13 @@ int yStart = 1;
 
 
 void tuiMain(){
+    startupWindow();
+    /*
     startTUI();
     bufferedWriting();
     stopTUI();
+    */
+    
 }
 
 void startTUI(){
@@ -132,4 +137,44 @@ void printStatus(){
 
     mvwprintw(statWin, 1, xStart, "# Active Users: \t%d", numUsers);
     mvwprintw(statWin, 2, xStart, "# Discovery Keyword:\t%s", discoveryStr);
+}
+
+
+int startupWindow(){
+    initscr(); cbreak(); noecho(); // Inital setup of screen
+    keypad(stdscr, TRUE); // Enables navigation with keyboard
+    curs_set(0);
+
+    WINDOW* startupWin = newwin(7, 30, 0, 0);
+    box(startupWin, 0, 0);
+    refresh();
+    
+    mvwprintw(startupWin, 1, 1, "Please enter '0' or '1'");
+    mvwprintw(startupWin, 3, 1, "0:\tHost Server");
+    mvwprintw(startupWin, 4, 1, "1:\tJoin Server");
+    mvwprintw(startupWin, 5, 1, "2:\tExit");
+    wrefresh(startupWin);
+
+    int returnStatus = -1;
+    while(true){
+        char c = getch();
+        if (c == '0'){
+            returnStatus = 0; // Host
+            break;
+        }
+        if (c == '1'){
+            returnStatus = 1; // Join
+            break;
+        }
+        if (c == '2'){
+            returnStatus = -1; // Exit
+            break;
+        }
+    }
+
+    werase(startupWin);
+    delwin(startupWin);
+    endwin();
+
+    return returnStatus;
 }
