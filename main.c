f63eb25b26c576f79ab009f14bb91573c44c80dc
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-11-09 14:46:46 +0100

    Initial setup of combining discover with tui

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include "common.h"
 
 int main(void) {
-    tuiMain();
-    return 0;
+    srand(time(0));
+
+    int returnStatus = startupWindow();
+    switch (returnStatus)
+    {
+    case 0: // Host
+        // Discover
+        char* pass_phrase = generate_pass_phrase();
+        run_listener();
+        // Start TUI
+        startTUI(pass_phrase);
+        bufferedWriting();
+        stopTUI();
+
+        break;
+    case 1: // Join
+        // Input Keyword
+        // Connect
+        // Start TUI
+        break;
+
+    default: // Exit
+        exit(EXIT_SUCCESS);
+        break;
+    }
+
+    return EXIT_SUCCESS;
 }
