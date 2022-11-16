b95a9ea31c5404055107890cba298dea49c35597
mikael <mikael.fangel@pm.me>
2022-11-16 16:41:26 +0100

    initial commit for serializer

--- parser.c
+++ parser.c
@@ -1,3 +1,6 @@
+#include "common.h"
+#include <string.h>
+
 Message* parser(char* string) 
 {
 
@@ -5,5 +8,17 @@ Message* parser(char* string)
 
 char* serialize(Message* message) 
 {
+    char *result = (char *) calloc(1, sizeof(*message) + 100);
+    result[0] = '{';
+    result[1] = '\n';
+    result[2] = '\0';
+    char conv[5];
+    sprintf(conv, "%d\n", message->x);
+    strcat(result, conv); 
+    sprintf(conv, "%d\n", message->y);
+    strcat(result, conv); 
+    strcat(result, message->message);
+    strcat(result, "\n}");
 
+    return result;
 }
