821dfb30173993d57755d6675e607116ea2bccb5
Christian <s133288@dtu.dk>
2022-11-17 10:52:42 +0100

    working on miss reading the chars when writing on second line

--- linkedlist_test.c
+++ linkedlist_test.c
@@ -1,5 +1,17 @@
 #include "common.h"
 #include "linklist.h"
+#include <string.h>
+
+void assert_equal(char* string1, char* string2)
+{
+	if(!strcmp(string1,string2))
+	{
+		printf("test passed\n");
+		return;
+	}
+	printf("test failed - reason: ");
+	printf("tryed to compare %s - %s\n", string1, string2);
+}
 
 int main(void){
 	
@@ -10,46 +22,39 @@ int main(void){
 	char** test = get_all_lines();
 	int num_test = 0;
 	
-	printf("%s\n", test[0]);
-	
 	for(int i = 0; i < 5; i++){
 		write_char(i,'a'+i);
 	}
 	delete_char(1);
 	
 	test = get_all_lines();
-
-	printf("%s\n", test[0]);
+	assert_equal(test[0], "bcde");
 	
 	delete_char(6);
 
 	test = get_all_lines();
-	printf("%s\n", test[0]);
+	assert_equal(test[0], "bcde");
 	
 	delete_char(4);
 
 	test = get_all_lines();
-	printf("%s\n", test[0]);
+	assert_equal(test[0], "bcd");
 	
 	
 	write_char(3,'\n');
 	
-	
 	clicked_on_line(1);
 	
 	test = get_all_lines();
-	num_test = get_amount_of_lines();
-	printf("%d\n", num_test);
-	printf("%s\n", test[0]);
-	printf("%s\n", test[1]);
-	/*
-	write_char(0,'a');
+	assert_equal(test[0], "bcd");
+	assert_equal(test[1], "");
+	
+	write_char(0,'k');
 	
 	test = get_all_lines();
-	printf("%s\n", test[0]);
-	if(test[1])
-		printf("%s\n", test[1]);
+	assert_equal(test[0], "bcd");
+	assert_equal(test[1], "k");
 	
-	*/
+	//clicked_on_line(0);
 
 }
