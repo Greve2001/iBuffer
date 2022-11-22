b1b0c6628f1b207d66c8a3346a6ea1603150f93e
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-11-22 09:33:24 +0100

    FIX set ALT_NEWLINE to niche symbol

--- common.h
+++ common.h
@@ -34,7 +34,7 @@
 #define LEFT_ARROW          4
 #define RIGHT_ARROW         5
 #define RETURN              7
-#define ALT_NEWLINE			'x'
+#define ALT_NEWLINE			'`'
 
 // Structs
 struct Active_Line; //Forward decleration
